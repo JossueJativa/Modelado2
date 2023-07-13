162baf4789d680cdbbe789d2b62ddac4d32de52f
Jossue Jativa <jativatamayojossuedavid@gmail.com>
2023-07-13 01:07:30 -0500

    Ingreso de trabajadores y movimientos

--- modelado2/recursos/urls.py
+++ modelado2/recursos/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('pagCentroCostos', views.pagCentroCostos, name='pagCentroCostos'),
     path('centrocostos', views.centrocostos, name='centrocostos'),
     path('pagTipoTrabajador', views.pagTipoTrabajador, name='pagTipoTrabajador'),
     path('pagTipoContrato', views.pagTipoContrato, name='pagTipoContrato'),
@@ -12,4 +13,19 @@ urlpatterns = [
     path('pagTipoCese', views.pagTipoCese, name='pagTipoCese'),
     path('pagEstadoTrabajador', views.pagEstadoTrabajador, name='pagEstadoTrabajador'),
     path('pagTipoTrabajador', views.pagTipoTrabajador, name='pagTipoTrabajador'),
+    path('agregarCentroCostos', views.agregarCentroCostos, name='agregarCentroCostos'),
+    path('eliminarCentroCostos/<str:id>/<str:descripcion>', views.eliminarCentroCostos, name='eliminarCentroCostos'),
+    path('editCentroCostos/<str:id>', views.editCentroCostos, name='editCentroCostos'),
+    path('edit/<str:id>', views.edit, name='edit'),
+
+    ##Movimiento de plantilla
+    path('PagMovimientoPlanilla', views.PagMovimientoPlanilla, name='PagMovimientoPlanilla'),
+    path('PagMovimientoPlanillaSearch', views.PagMovimientoPlanillaSearch, name='PagMovimientoPlanillaSearch'),
+    path('PagMovimientoPlanillaEdit/<str:id>', views.PagMovimientoPlanillaEdit, name='PagMovimientoPlanillaEdit'),
+    path('editMovimientoPlantilla/<str:id>', views.editMovimientoPlantilla, name='editMovimientoPlantilla'),
+    path('PagMovimientoPlanillaCreate', views.PagMovimientoPlanillaCreate, name='PagMovimientoPlanillaCreate'),
+    path('pushMovimientoPlantilla', views.pushMovimientoPlantilla, name='pushMovimientoPlantilla'),
+    path('eliminarMovimientoPlanilla/<str:id>', views.eliminarMovimientoPlanilla, name='eliminarMovimientoPlanilla'),
+
+    ##Trabajador
 ]
--- modelado2/recursos/views.py
+++ modelado2/recursos/views.py
@@ -12,7 +12,7 @@ def index(request):
 
         #Verificar ingreso de usuario que no ingrese letras
         if username.isalpha() == True:
-            return render(request, 'index.html', {
+            return render(request, 'recursos/index.html', {
                 'error': "Usuario o contraseña incorrectos",
                 'emisores': emisores,
             })
@@ -22,7 +22,7 @@ def index(request):
                                username+"&password="+password)
             data = url.json()
         except:
-            return render(request, 'index.html', {
+            return render(request, 'recursos/index.html', {
                 'error': "Usuario o contraseña incorrectos",
                 'emisores': emisor,
             })
@@ -36,7 +36,7 @@ def index(request):
                     'data': data,
                 })   
             else:
-                return render(request, 'index.html', {
+                return render(request, 'recursos/index.html', {
                     'error': "Emisor mal registrado",
                     'emisores': emisor,
                 })
@@ -59,7 +59,8 @@ def centrocostos(request):
             'codigo': codigo,
             'descripcion': descripcionmostrar,
         })
-
+    
+def pagCentroCostos(request):
     url = requests.get("http://apiservicios.ecuasolmovsa.com:3009/api/Varios/CentroCostosSelect")
     data = url.json()
 
@@ -114,3 +115,275 @@ def pagEstadoTrabajador(request):
     return render(request, 'recursos/estadotrabajador.html',{
         'data': data,
     })
+
+#recursos/
+def agregarCentroCostos(request):
+    if request.method == 'POST':
+        descripcion = request.POST['descripcion']
+        codigo = request.POST['codigo']
+        
+        requests.post("http://apiservicios.ecuasolmovsa.com:3009/api/Varios/CentroCostosInsert?descripcioncentrocostos="+descripcion+"&codigocentrocostos="+codigo)
+        url = requests.get("http://apiservicios.ecuasolmovsa.com:3009/api/Varios/CentroCostosSelect")
+        data = url.json()
+
+        return render(request, 'recursos/centrocostos.html',{
+            'data': data,
+        })
+    else:
+        return render(request, 'recursos/agregarCentroCostos.html')
+
+def eliminarCentroCostos(request, id, descripcion):
+    if request.method == 'POST':
+        try:
+            url = requests.get("http://apiservicios.ecuasolmovsa.com:3009/api/Varios/CentroCostosDelete?codigocentrocostos="+id+"&descripcioncentrocostos="+descripcion)
+        except:
+            return render(request, 'recursos/centrocostos.html', {
+                'message': "Error al eliminar el centro de costos"
+            })
+        
+        url = requests.get("http://apiservicios.ecuasolmovsa.com:3009/api/Varios/CentroCostosSelect")
+        data = url.json()
+        return render(request, 'recursos/centrocostos.html',{
+                'data': data,
+            })
+    else:
+        url = requests.get("http://apiservicios.ecuasolmovsa.com:3009/api/Varios/CentroCostosSelect")
+        data = url.json()
+        return render(request, 'recursos/centrocostos.html',{
+                'data': data,
+            })
+            
+def editCentroCostos(request, id):
+    url = requests.get("http://apiservicios.ecuasolmovsa.com:3009/api/Varios/CentroCostosSelect")
+    data = url.json()
+
+    for datos in data:
+        if datos['Codigo'] == int(id):
+            data = datos
+    
+    return render(request, 'recursos/editCentroCostos.html',{
+        'data': data,
+    })
+
+def edit(request, id):
+    if request.method == "POST":
+        descripcion = request.POST['descripcion']
+
+        try:
+            url = requests.get("http://apiservicios.ecuasolmovsa.com:3009/api/Varios/CentroCostosUpdate?codigocentrocostos="+id+"&descripcioncentrocostos="+descripcion)
+
+        except:
+            return render(request, 'exceptiondentro.html', {
+                'message': "Error al editar el centro de costos"
+            })
+        
+        url = requests.get("http://apiservicios.ecuasolmovsa.com:3009/api/Varios/CentroCostosSelect")
+        data = url.json()
+        return render(request, 'recursos/centrocostos.html',{
+                'data': data,
+            })
+    else:
+        url = requests.get("http://apiservicios.ecuasolmovsa.com:3009/api/Varios/CentroCostosSelect")
+        data = url.json()
+        return render(request, 'recursos/centrocostos.html',{
+                'data': data,
+            })
+    
+
+##Movimiento de plantilla
+def PagMovimientoPlanilla (request):
+    url = requests.get("http://apiservicios.ecuasolmovsa.com:3009/api/Varios/MovimientoPlanillaSelect")
+    data = url.json()
+
+    ##Filtrar por prioridad
+    data = sorted(data, key=lambda k: k['Prioridad'], reverse=True)
+    
+    return render(request, 'movimientoplanilla.html',{
+        'data': data,
+    })
+
+def PagMovimientoPlanillaSearch (request):
+    if request.method == "POST":
+        ## buscar por Concepto
+        concepto = request.POST['Concepto']
+        ##Quitar los espacios entre palabras
+        concepto = concepto.replace(" ", "&")
+
+        url = requests.get("http://apiservicios.ecuasolmovsa.com:3009/api/Varios/MovimientoPlanillaSearch?Concepto="+concepto)
+        data = url.json()
+
+        ##Filtrar por prioridad
+        data = sorted(data, key=lambda k: k['Prioridad'], reverse=True)
+        
+        return render(request, 'busquedaMovimientoPlantilla.html',{
+            'data': data,
+        })
+    
+def eliminarMovimientoPlanilla (request, id):
+    if request.method == 'POST':
+
+        url = requests.get("http://apiservicios.ecuasolmovsa.com:3009/api/Varios/MovimientoPlanillaSelect")
+        data = url.json()
+
+        # buscar por Concepto
+        for datos in data:
+            if datos['CodigoConcepto'] == int(id):
+                concepto = datos['Concepto']
+
+        url = requests.delete("http://apiservicios.ecuasolmovsa.com:3009/api/Varios/MovimeintoPlanillaDelete?codigomovimiento="+id+
+                            "&descripcionomovimiento="+concepto)
+        
+        url = requests.get("http://apiservicios.ecuasolmovsa.com:3009/api/Varios/MovimientoPlanillaSelect")
+        data = url.json()
+
+        ##Filtrar por prioridad
+        data = sorted(data, key=lambda k: k['Prioridad'], reverse=True)
+
+        return render(request, 'movimientoplanilla.html',{
+                'data': data,
+            })
+
+    
+def PagMovimientoPlanillaEdit (request, id):
+    url = requests.get("http://apiservicios.ecuasolmovsa.com:3009/api/Varios/MovimientoPlanillaSelect")
+    data = url.json()
+
+    for datos in data:
+        if datos['CodigoConcepto'] == int(id):
+            data = datos
+
+    ##Sacar tipos de operaciones
+    url2 = requests.get("http://apiservicios.ecuasolmovsa.com:3009/api/Varios/TipoOperacion")
+    data2 = url2.json()
+
+    ##Sacar movimiento excepcion
+    url3 = requests.get("http://apiservicios.ecuasolmovsa.com:3009/api/Varios/MovimientosExcepcion1y2")
+    data3 = url3.json()
+
+    #Sacar movimiento de excepcion 3
+    url4 = requests.get("http://apiservicios.ecuasolmovsa.com:3009/api/Varios/MovimientosExcepcion3")
+    data4 = url4.json()
+
+    ##Sacar traba aplica a iess
+    url5 = requests.get("http://apiservicios.ecuasolmovsa.com:3009/api/Varios/TrabaAfectaIESS")
+    data5 = url5.json()
+
+    ##Sacar afecta impuesto a la renta
+    url6 = requests.get("http://apiservicios.ecuasolmovsa.com:3009/api/Varios/TrabAfecImpuestoRenta")
+    data6 = url6.json()
+    
+    return render(request, 'editMovimientoPlanilla.html',{
+        'datos': data,
+        'operacion': data2,
+        'excepcion': data3,
+        'excepcion3': data4,
+        'traba': data5,
+        'afecta': data6,
+    })
+
+def editMovimientoPlantilla (request, id):
+    if request.method == "POST":
+        Conceptos = request.POST['Conceptos']
+        Prioridad = request.POST['Prioridad']
+        TipoOperacion = request.POST['TipoOperacion']
+        Cuenta1 = request.POST['Cuenta1']
+        Cuenta2 = request.POST['Cuenta2']
+        Cuenta3 = request.POST['Cuenta3']
+        Cuenta4 = request.POST['Cuenta4']
+        MovimientoExcepcion1 = request.POST['MovimientoExcepcion1']
+        MovimientoExcepcion2 = request.POST['MovimientoExcepcion2']
+        MovimientoExcepcion3 = request.POST['MovimientoExcepcion3']
+        Traba_Aplica_iess = request.POST['Traba_Aplica_iess']
+        Traba_Proyecto_imp_renta = request.POST['Traba_Proyecto_imp_renta']
+        Aplica_Proy_Renta = request.POST['Aplica_Proy_Renta']
+        Empresa_Afecta_Iess = request.POST['Empresa_Afecta_Iess']
+
+        try:
+            requests.get("http://apiservicios.ecuasolmovsa.com:3009/api/Varios/MovimientoPlanillaUpdate?codigoplanilla="+id+"&conceptos="+Conceptos+"&prioridad="+Prioridad+"&tipooperacion="+TipoOperacion+"&cuenta1="+Cuenta1+"&cuenta2="+Cuenta2+"&cuenta3="+Cuenta3+"&cuenta4="+Cuenta4+"&MovimientoExcepcion1="+MovimientoExcepcion1+"&MovimientoExcepcion2="+MovimientoExcepcion2+"&MovimientoExcepcion3="+MovimientoExcepcion3+"&Traba_Aplica_iess="+Traba_Aplica_iess+"&Traba_Proyecto_imp_renta="+Traba_Proyecto_imp_renta+"&Aplica_Proy_Renta="+Aplica_Proy_Renta+"&Empresa_Afecta_Iess="+Empresa_Afecta_Iess)
+        except:
+            return render(request, 'exceptiondentro.html',{
+                'message': 'Error al actualizar el movimiento de planilla',
+            })
+
+        url = requests.get("http://apiservicios.ecuasolmovsa.com:3009/api/Varios/MovimientoPlanillaSelect")
+        data = url.json()
+
+        ##Filtrar por prioridad
+        data = sorted(data, key=lambda k: k['Prioridad'], reverse=True)
+
+        return render(request, 'movimientoplanilla.html',{
+            'data': data,
+        })
+    
+def PagMovimientoPlanillaCreate (request):
+    ##Sacar tipos de operaciones
+    url2 = requests.get("http://apiservicios.ecuasolmovsa.com:3009/api/Varios/TipoOperacion")
+    data2 = url2.json()
+
+    ##Sacar movimiento excepcion
+    url3 = requests.get("http://apiservicios.ecuasolmovsa.com:3009/api/Varios/MovimientosExcepcion1y2")
+    data3 = url3.json()
+
+    #Sacar movimiento de excepcion 3
+    url4 = requests.get("http://apiservicios.ecuasolmovsa.com:3009/api/Varios/MovimientosExcepcion3")
+    data4 = url4.json()
+
+    ##Sacar traba aplica a iess
+    url5 = requests.get("http://apiservicios.ecuasolmovsa.com:3009/api/Varios/TrabaAfectaIESS")
+    data5 = url5.json()
+
+    ##Sacar afecta impuesto a la renta
+    url6 = requests.get("http://apiservicios.ecuasolmovsa.com:3009/api/Varios/TrabAfecImpuestoRenta")
+    data6 = url6.json()
+
+    return render(request, 'agregarMovimiento.html',{
+        'operacion': data2,
+        'excepcion': data3,
+        'excepcion3': data4,
+        'traba': data5,
+        'afecta': data6,
+    })
+
+def pushMovimientoPlantilla(request):
+    if request.method == "POST":
+        Conceptos = request.POST['Conceptos']
+        Prioridad = request.POST['Prioridad']
+        TipoOperacion = request.POST['TipoOperacion']
+        Cuenta1 = request.POST['Cuenta1']
+        Cuenta2 = request.POST['Cuenta2']
+        Cuenta3 = request.POST['Cuenta3']
+        Cuenta4 = request.POST['Cuenta4']
+        MovimientoExcepcion1 = request.POST['MovimientoExcepcion1']
+        MovimientoExcepcion2 = request.POST['MovimientoExcepcion2']
+        MovimientoExcepcion3 = request.POST['MovimientoExcepcion3']
+        Traba_Aplica_iess = request.POST['Traba_Aplica_iess']
+        Traba_Proyecto_imp_renta = request.POST['Traba_Proyecto_imp_renta']
+        Aplica_Proy_Renta = request.POST['Aplica_Proy_Renta']
+        Empresa_Afecta_Iess = request.POST['Empresa_Afecta_Iess']
+
+        Traba_Aplica_iess = Traba_Aplica_iess + " Aplica"
+
+        if Traba_Proyecto_imp_renta == "Aplica":
+            Traba_Proyecto_imp_renta = "Si Aplica"
+
+        try:
+            requests.post("http://apiservicios.ecuasolmovsa.com:3009/api/Varios/MovimientoPlanillaInsert?conceptos="+Conceptos+
+                         "&prioridad="+Prioridad+"&tipooperacion="+TipoOperacion+"&cuenta1="+Cuenta1+"&cuenta2="+Cuenta2+
+                         "&cuenta3="+Cuenta3+"&cuenta4="+Cuenta4+"&MovimientoExcepcion1="+MovimientoExcepcion1+
+                         "&MovimientoExcepcion2="+MovimientoExcepcion2+"&MovimientoExcepcion3="+MovimientoExcepcion3+
+                         "&Traba_Aplica_iess="+Traba_Aplica_iess+"&Traba_Proyecto_imp_renta="+Traba_Proyecto_imp_renta+
+                         "&Aplica_Proy_Renta="+Aplica_Proy_Renta+"&Empresa_Afecta_Iess="+Empresa_Afecta_Iess)
+        except:
+            return render(request, 'exceptiondentro.html',{
+                'message': 'Error al insertar el movimiento de planilla',
+            })
+
+        url = requests.get("http://apiservicios.ecuasolmovsa.com:3009/api/Varios/MovimientoPlanillaSelect")
+        data = url.json()
+
+        ##Filtrar por prioridad
+        data = sorted(data, key=lambda k: k['Prioridad'], reverse=True)
+
+        return render(request, 'movimientoplanilla.html',{
+            'data': data,
+        }) 
